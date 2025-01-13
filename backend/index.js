85964f8ecf30349ba1a2be190541790712f901db
Pranjal360Agarwal <pranjal360agarwal@gmail.com>
2025-01-13 16:49:44 +0530

    Setup the backend part

---
+++ backend/index.js
@@ -0,0 +1,11 @@
+import express from "express";
+import http from "http";
+
+const app = express();
+
+const server = http.createServer(app);
+
+const port = process.env.PORT || 5000;
+server.listen(port, () => {
+    console.log("server is working on port 5000");
+});
